c4a03a02e7930ee410a640b7d4b6abd76072e4db
Juliana <juliana.jeanmarie972@gmail.com>
2018-09-16 00:14:21 +0200

    form add

--- src/components/add-todo.js
+++ src/components/add-todo.js
@@ -3,25 +3,39 @@ import { connect } from 'react-redux';
 import { func } from 'prop-types';
 import { addTodo } from '../actions/add-todo';
 
-const AddTodo = ({ addTodo }) => {
-  let input;
-  const onSubmit = (e) => {
+class AddTodo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { task: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ task: event.target.value });
+  }
+
+  handleSubmit(e) {
     e.preventDefault();
-    if (!input.value.trim()) {
+    if (!this.state.task.trim()) {
       return;
     }
-    addTodo(input.value);
-    input.value = '';
-  };
-  return (
-    <div>
-      <form onSubmit={onSubmit} >
-        <input ref={node => input = node} />
-        <button type="submit">Create new task</button>
-      </form>
-    </div>
-  );
-};
+    this.props.addTodo(this.state.task);
+    this.setState({ task: '' });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit} >
+          <input type="text" value={this.state.task} onChange={this.handleChange} />
+          <input type="submit" value="Create new task" />
+        </form>
+      </div>
+    );
+  }
+}
 
 AddTodo.propTypes = {
   addTodo: func.isRequired,
