8bea695d5c2c01524b6601b06eac89a8f3bf6e69
Juliana <juliana.jeanmarie972@gmail.com>
2018-09-17 15:07:10 +0200

    action-bar

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { shape, number, string, func, arrayOf, bool } from 'prop-types';
 import { Todo } from './todo';
 import { updateTodo, removeTodo } from '../actions/todo';
 
 const TodoList = ({ todos, updateTodo, removeTodo }) => (
-  <ul className="todo-background">
-    {todos.map(todo => (
-      <Todo
-        key={todo.id}
-        todo={todo}
-        onClick={{ updateTodo, removeTodo }}
-      />))
-      }
-  </ul>);
+  <Fragment>
+    <div className="action-bar">
+      <button type="button">show completed</button>
+      <button type="button">remove all</button>
+    </div>
+    <br />
+    <ul className="todo-background">
+      {todos.map(todo => (
+        <Todo
+          key={todo.id}
+          todo={todo}
+          onClick={{ updateTodo, removeTodo }}
+        />))
+        }
+    </ul>
+  </Fragment>);
 
 TodoList.propTypes = {
   todos: arrayOf(shape({
